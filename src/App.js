ad650a9e4241c51f805dca01fc4d13a47bb72b41
Richard Win <l1828356181@gmail.com>
2025-11-26 22:41:07 +0800

    组件通信与插槽 React 插槽， 闭合标签传值，父组件向子组件传值 终

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-function Detail({content, active}) {
+function List({children, title, footer=<p>这是我的默认的footer</p>}) {
   return (
     <>
-      <p>{content}</p>
-      <p>状态: {active ? "显示中" : "已隐藏"}</p>
+        <p>{title}</p>
+        <ul>
+          {children}
+        </ul>
+        {footer}
     </>
-  );
+
+  )
 }
 
-function Article({ title, detailData }) {
-  return (
-    <div>
-      <h2>{title}</h2>
-      <Detail {...detailData}/>
-    </div>
-  );
-}
 
 export default function App() {
-  const articleData = {
-    title: '标题2',
-    detailData: {
-      content: '内容1',
-      active: true
-    }
-  }
+
   return (
     <>
-      <Article {...articleData} />
+      <List
+        title="列表1"
+        footer={<p>这是我的单独的footer</p>}
+      >
+        <li>列表项1</li>
+        <li>列表项1</li>
+        <li>列表项1</li>
+      </List>
+      <List
+        title="列表2"
+      >
+        <li>列表项2</li>
+        <li>列表项2</li>
+        <li>列表项2</li>
+      </List>
     </>
   );
 }
+
+//JSX作为Props来传递
