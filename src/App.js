83a61bcbc5d157a77c541a6721294715a19a71da
richard <l1828356181@gmail.com>
2025-11-26 16:57:34 +0800

    组建通信与插槽part1 React DOM Props设置

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 import { Fragment } from "react/jsx-runtime";
-
+import image from "./logo.svg";
 function App() {
-  const divContent = "标签内容";
-  const divTitle = "标签标题";
+  const imgStyleObj = {
+    width: 200, //这里默认单位是px
+    //width: "100vh"
+    height: 200,
+    backgroundColor: "grey",
+  };
 
-  const flag = false;
-  let content = null;
+  const imgData = {
+    className: 'hi',
+    style: {
+      width: 200,
+      height: 200,
+      backgroundColor: 'grey'
+    }
 
-  
-  if (flag) {
-    content = <span>flag是true</span>;
-  } else {
-    content = <p>flag是false</p>;
-  }
-
-  const list = [
-    { id: 1, name: "小路" },
-    { id: 2, name: "小丽" },
-    { id: 3, name: "小查" },
-  ];
-
-  const listContent = list.map((item) => (
-    <Fragment key={item.id}>
-      <li>{item.name}</li>
-      <span>cnm</span>
-    </Fragment>
-  ));
-
-  function handleClick(e) {
-    console.log('点击了按钮', e);
-    
   }
   return (
     <>
-      <div title={divTitle}>{divContent}</div>
-      <div>{content}</div>
-      <ul>{listContent}</ul>
-      <button onClick={handleClick}>按钮:更改flag的属性</button>
+      <div>
+        <img
+          src={image}
+          alt=""
+          className="small"
+          style={imgStyleObj}
+        ></img>
+      </div>
+
+      <div>
+        <img
+        src={image}
+        alt=""
+        {...imgData}></img>
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Test from './useState测试数组内容';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Test />
+    <App />
   </React.StrictMode>
 );
 
